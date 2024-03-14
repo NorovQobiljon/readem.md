2c02dad43325899a3ce21fd3005a629d7f24371c
Пользователь <qobiljonnorov81@gmail.com>
2024-03-14 06:49:07 +0500

    vazifa tugadi

---
+++ script.js
@@ -0,0 +1,90 @@
+// [1. How Edabit Works] (https://edabit.com/challenge/ARr5tA458o2tC9FTN);
+//
+function hello() {
+    return "hello edabit.com"
+}
+
+//
+
+// [2. How Edabit Works] (https://edabit.com/challenge/3LpBLgNRyaHMvNb4j);
+//
+
+function addition(a, b) {
+    return a + b
+}
+
+//
+
+// [3. How Ebdait Works](https://edabit.com/challenge/8q54MKnRrm89pSLmW);
+//
+
+function convert(minutes) {
+    return minutes * 60
+}
+
+//
+
+// [4. How Ebdait Works](https://edabit.com/challenge/NAQhEoxbofPidLxm9);
+//
+
+function addition(num) {
+    return ++num
+}
+
+//
+
+// [5. How Ebdait Works](https://edabit.com/challenge/3CaszbdZYGN4otQD8);
+//
+
+function triArea(base, height) {
+    return base * height / 2
+}
+
+//
+
+// [6. How Ebdait Works](https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw);
+//
+
+function calcAge(age) {
+	return age * 365
+}
+
+//
+// [7. How Ebdait Works](https://edabit.com/challenge/j7yQbF3J3rToHsDBP);
+//
+
+function cubes(a) {
+	return a ** 3
+}
+
+//
+// [8. How Ebdait Works](https://edabit.com/challenge/QaApgtePE6QrCZ64o);
+//
+
+function getFirstValue(arr) {
+	return arr[0]
+}
+
+//
+// [9. How Ebdait Works](https://edabit.com/challenge/wAdE9te55cowBLcPs);
+//
+
+function circuitPower(voltage, current) {
+	return voltage * current
+}
+
+//
+// [10. How Ebdait Works](https://edabit.com/challenge/6AnQqiEjkJdZrWhPS);
+//
+
+function howManySeconds(hours) {
+	return hours * 3600
+}
+
+//  
+
+
+
+
+
+
